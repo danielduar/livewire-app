871bb5dc1b8996d45a3aa74c9e384d2fae6a1a7d
Daniel <dlandim41@gmail.com>
2022-01-08 22:43:18 -0300

    Add the Jetstream Autentication

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('tweets', ShowTweets::class);
+Route::get('tweets', ShowTweets::class)->middleware('auth','verified');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
